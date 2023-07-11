5f54de3507baf809b795b4d35ca01874fc24681a
PencilTorch <furyaxe@yandex.ru>
2023-07-11 16:00:41 +0300

    add sstream.cpp

---
+++ sstream.cpp
@@ -0,0 +1,23 @@
+﻿// http://www.itmathrepetitor.ru/s-lekciya-9-rabota-s-potokami-v-s/
+// https://pro-prof.com/forums/topic/cpp_files_io
+// https://prog-cpp.ru/cpp-files/
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ostringstream oss;
+    string s;
+    oss.str("Sample string_1.");
+    s = oss.str();
+    cout << s << endl;
+    oss << "Sample string_2.";
+
+    s = oss.str();
+    cout << s << endl;
+
+    return 0;
+}
