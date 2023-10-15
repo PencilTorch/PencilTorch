a12d6c5845c2bd75a541ed0c4c02a7fcd00b30f8
Maksim <83720131+PencilTorch@users.noreply.github.com>
2023-10-15 21:33:21 +0300

    Update AntiSlipe.cpp

--- AntiSlipe.cpp
+++ AntiSlipe.cpp
@@ -1,21 +1,36 @@
-﻿
 #define WINDOWS_LEAN_AND_MEAN
 #include <Windows.h>
 #include <iostream>
 
 // do something after 4 minutes of user inactivity
-static const unsigned int idle_milliseconds = 60 * 4 * 1000;
+static const unsigned int idle_milliseconds = 60 * 1 * 1000;
 
 void moveMouse(int x, int y) {
-    // set mouse at center screen
-    SetCursorPos(x / 2, y / 2);
-    Sleep(1);
-    SetCursorPos((x / 2) + 10, (y / 2) + 10);
+    int step = 10;
+    for (int i = 0; i < 30; i++) {
+        Sleep(100);
+        SetCursorPos(x += step, y);
+    }
+    for (int i = 0; i < 30; i++) {
+        Sleep(100);
+        SetCursorPos(x, y += step);
+    }
+    for (int i = 0; i < 30; i++) {
+        Sleep(100);
+        SetCursorPos(x -= step, y);
+    }
+    for (int i = 0; i < 30; i++) {
+        Sleep(100);
+        SetCursorPos(x, y -= step);
+    }
 }
 
 int main() {
     HWND hConsole = GetConsoleWindow();//Если компилятор старый заменить на GetForegroundWindow()
     ShowWindow(hConsole, SW_HIDE);//собственно прячем оконо консоли
+
+    DWORD currentTime = GetTickCount();
+
     LASTINPUTINFO last_input;
     last_input.cbSize = sizeof(LASTINPUTINFO);
     int Height = GetSystemMetrics(SM_CYSCREEN);
@@ -27,21 +42,22 @@ int main() {
             std::cerr << "WinAPI failed!" << std::endl;
             return 0;
         }
-
+        last_input.dwTime = last_input.dwTime - currentTime;
         if (last_input.dwTime < idle_milliseconds) {
             // user hasn't been idle for long enough
             Sleep(1000);
             continue;
         }
-
+        // set mouse at center screen
+        SetCursorPos(Width / 2, Height / 2);
         // user has been idle at least 4 minutes
-        moveMouse(Width, Height);
+        moveMouse(Width / 2, Height / 2);
         // done. Wait before doing the next loop.
         Sleep(idle_milliseconds);
     }
 
 
-    
-    
+
+
     return 0;
 }
