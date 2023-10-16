2cd74bf3cd75fcb8dafbbd6c192757e0a5846956
PencilTorch <furyaxe@yandex.ru>
2023-10-16 15:59:08 +0300

    update AntiSlipe.cpp mouse simulator

--- AntiSlipe.cpp
+++ AntiSlipe.cpp
@@ -2,26 +2,21 @@
 #include <Windows.h>
 #include <iostream>
 
-// do something after 4 minutes of user inactivity
+// do something after any minutes of user inactivity
 static const unsigned int idle_milliseconds = 60 * 1 * 1000;
 
 void moveMouse(int x, int y) {
-    int step = 10;
-    for (int i = 0; i < 30; i++) {
-        Sleep(100);
-        SetCursorPos(x += step, y);
-    }
-    for (int i = 0; i < 30; i++) {
-        Sleep(100);
-        SetCursorPos(x, y += step);
-    }
-    for (int i = 0; i < 30; i++) {
-        Sleep(100);
-        SetCursorPos(x -= step, y);
-    }
-    for (int i = 0; i < 30; i++) {
-        Sleep(100);
-        SetCursorPos(x, y -= step);
+    int count = 100;
+    int movex, movey;
+    float angle = 0.0f;
+    // set mouse at center screen
+    SetCursorPos(x / 2, y / 2);
+    for (int i = 0; i <= count; i++) {
+        angle = .3 * i;
+        movex = (angle * cos(angle) * 2) + x / 2;
+        movey = (angle * sin(angle) * 2) + y / 2;
+        SetCursorPos(movex, movey);
+        Sleep(1);
     }
 }
 
@@ -29,8 +24,7 @@ int main() {
     HWND hConsole = GetConsoleWindow();//Если компилятор старый заменить на GetForegroundWindow()
     ShowWindow(hConsole, SW_HIDE);//собственно прячем оконо консоли
 
-    DWORD currentTime = GetTickCount();
-
+    DWORD currentTime;
     LASTINPUTINFO last_input;
     last_input.cbSize = sizeof(LASTINPUTINFO);
     int Height = GetSystemMetrics(SM_CYSCREEN);
@@ -38,20 +32,19 @@ int main() {
 
     // main loop to check if user has been idle long enough
     for (;;) {
+        currentTime = GetTickCount();
         if (!GetLastInputInfo(&last_input)) {
             std::cerr << "WinAPI failed!" << std::endl;
             return 0;
         }
-        last_input.dwTime = last_input.dwTime - currentTime;
+        currentTime > last_input.dwTime ? last_input.dwTime = currentTime - last_input.dwTime : last_input.dwTime = last_input.dwTime - currentTime;
         if (last_input.dwTime < idle_milliseconds) {
             // user hasn't been idle for long enough
             Sleep(1000);
             continue;
         }
-        // set mouse at center screen
-        SetCursorPos(Width / 2, Height / 2);
         // user has been idle at least 4 minutes
-        moveMouse(Width / 2, Height / 2);
+        moveMouse(Width, Height);
         // done. Wait before doing the next loop.
         Sleep(idle_milliseconds);
     }
