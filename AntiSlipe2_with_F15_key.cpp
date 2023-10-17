2517685ca3428ee20945ae3eb01d29f9df4dc842
PencilTorch <furyaxe@yandex.ru>
2023-10-17 15:01:24 +0300

    add AntiSlipe2_with_F15_key.cpp

---
+++ AntiSlipe2_with_F15_key.cpp
@@ -0,0 +1,19 @@
+﻿#include <Windows.h>
+#include <iostream>
+
+const unsigned int idle_milliseconds = 60 * 4 * 1000;
+
+
+int main() {
+    HWND hConsole = GetConsoleWindow();//Если компилятор старый заменить на GetForegroundWindow()
+    ShowWindow(hConsole, SW_HIDE);//собственно прячем оконо консоли
+
+    // main loop to check if user has been idle long enough
+    for (;;) {
+        keybd_event(VK_F15, 0, 0, 0);
+        keybd_event(VK_F15, 0, KEYEVENTF_KEYUP, 0);
+        Sleep(idle_milliseconds);
+    }
+
+    return 0;
+}
