6828d26fa42d8453c1f617469490c3102054dfa0
PencilTorch <borisb342@gmail.com>
2023-11-30 00:03:32 +0300

    add serialise_example.cpp

---
+++ serialise_example.cpp
@@ -0,0 +1,55 @@
+﻿
+
+
+
+
+#include <iostream>
+#include <fstream>
+
+class Kunde {
+public:
+    std::string familename;
+    std::string vorname;
+    int jhre;
+
+    friend std::ostream& operator<<(std::ostream & os, const Kunde& kunde) {
+    
+        os << kunde.familename << " " << kunde.vorname << " " << kunde.jhre;
+        return os;
+    }
+
+    friend std::ifstream& operator>>(std::ifstream& is, Kunde& kunde) {
+    
+        is >> kunde.familename >> kunde.vorname>> kunde.jhre;
+        return is;
+    }
+
+};
+
+int main()
+{
+    Kunde erstekunde;
+    erstekunde.familename = "Muller";
+    erstekunde.vorname = "Greta";
+    erstekunde.jhre = 25;
+    // Сериализация объекта в файл
+    std::ofstream outFile("kunden.dat");
+    outFile << erstekunde;
+    outFile.close();
+    // Десериализация объекта из файла
+    Kunde zweitekunde;
+    std::ifstream inFile("kunden.dat");
+    inFile >> zweitekunde;
+    inFile.close();
+
+    std::cout << "\n\n\t\tResult > "<< zweitekunde.familename<<", "<< zweitekunde.vorname << ", " << zweitekunde.jhre<<"\n\n";
+    system("pause");
+    return 0;
+}
+
+
+
+
+
+
+
