1f3f86475c0fe196ad7eafa18840bdb7135bd88d
PencilTorch <borisb342@gmail.com>
2024-03-29 13:09:07 +0300

    add make MD5-hashing word utility

---
+++ MakeMD5-hash_word/main.cpp
@@ -0,0 +1,24 @@
+﻿// MakeMD5.cpp : хэширование в MD5 и сохранение в файл
+//
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "MD5.h"
+
+
+int main() {
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+    std::string str = "Enter word for make MD5 hashing:";
+    std::cout << str << std::endl;
+    std::cin >> str;
+    std::string pwd = md5(str);
+    std::cout << pwd << std::endl;
+    std::ofstream fout("md5-hash_word");
+    if (fout.is_open()) {
+        fout << pwd;
+        std::cout << "the file with md5-hash word was created successfully!" << std::endl;
+    }
+    fout.close();
+
+    return 0;
+}
